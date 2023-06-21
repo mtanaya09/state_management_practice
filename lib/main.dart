427788cc09acdccfecb5dc88fc543c6d9e52225c
mtanaya09 <mtanaya09@gmail.com>
2023-06-22 05:36:18 +0800

    Comment the myscaffold

--- lib/main.dart
+++ lib/main.dart
@@ -24,3 +24,55 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+
+// {
+// 	"My Flutter Scaffold":{
+// 		"scope": "dart",
+// 		"body": 
+// 			["import 'package:flutter/material.dart';",
+// 			"void main() {",
+// 			"runApp(",
+// 			"MaterialApp(",
+// 			"title: 'Flutter Demo',",
+// 			"theme: ThemeData(",
+// 			"colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),",
+// 			"useMaterial3: true,",
+// 			"),",
+// 			"home: const HomePage(),",
+// 			"),",
+// 			");",
+// 			"}",
+			
+// 			"class HomePage extends StatelessWidget {",
+// 			"const HomePage({super.key});",
+// 			"@override",
+// 			"Widget build(BuildContext context) {",
+// 			"return Scaffold(",
+// 			"appBar: AppBar(",
+// 			"title: const Text('Home page'),",
+// 			"),",
+// 			");",
+// 			"}",
+// 			"}",
+// 		],
+// 		"prefix": "myscaffold",
+// 	}
+// 	// Place your global snippets here. Each snippet is defined under a snippet name and has a scope, prefix, body and 
+// 	// description. Add comma separated ids of the languages where the snippet is applicable in the scope field. If scope 
+// 	// is left empty or omitted, the snippet gets applied to all languages. The prefix is what is 
+// 	// used to trigger the snippet and the body will be expanded and inserted. Possible variables are: 
+// 	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. 
+// 	// Placeholders with the same ids are connected.
+// 	// Example:
+// 	// "Print to console": {
+// 	// 	"scope": "javascript,typescript",
+// 	// 	"prefix": "log",
+// 	// 	"body": [
+// 	// 		"console.log('$1');",
+// 	// 		"$2"
+// 	// 	],
+// 	// 	"description": "Log output to console"
+// 	// }
+// }
