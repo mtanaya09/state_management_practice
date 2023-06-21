bed6ab95f3a098ac3539679db8496f75f014781e
mtanaya09 <mtanaya09@gmail.com>
2023-06-22 06:12:48 +0800

    Setup the screens for adding contacts

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ void main() {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
       ),
       home: const HomePage(),
+      routes: {
+        '/new-contact': (context) => const NewContactView(),
+      },
     ),
   );
 }
@@ -17,62 +19,98 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home page'),
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
+class Contact {
+  final String name;
+  const Contact({
+    required this.name,
+  });
+}
 
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
 
-// {
-// 	"My Flutter Scaffold":{
-// 		"scope": "dart",
-// 		"body": 
-// 			["import 'package:flutter/material.dart';",
-// 			"void main() {",
-// 			"runApp(",
-// 			"MaterialApp(",
-// 			"title: 'Flutter Demo',",
-// 			"theme: ThemeData(",
-// 			"colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),",
-// 			"useMaterial3: true,",
-// 			"),",
-// 			"home: const HomePage(),",
-// 			"),",
-// 			");",
-// 			"}",
-			
-// 			"class HomePage extends StatelessWidget {",
-// 			"const HomePage({super.key});",
-// 			"@override",
-// 			"Widget build(BuildContext context) {",
-// 			"return Scaffold(",
-// 			"appBar: AppBar(",
-// 			"title: const Text('Home page'),",
-// 			"),",
-// 			");",
-// 			"}",
-// 			"}",
-// 		],
-// 		"prefix": "myscaffold",
-// 	}
-// 	// Place your global snippets here. Each snippet is defined under a snippet name and has a scope, prefix, body and 
-// 	// description. Add comma separated ids of the languages where the snippet is applicable in the scope field. If scope 
-// 	// is left empty or omitted, the snippet gets applied to all languages. The prefix is what is 
-// 	// used to trigger the snippet and the body will be expanded and inserted. Possible variables are: 
-// 	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. 
-// 	// Placeholders with the same ids are connected.
-// 	// Example:
-// 	// "Print to console": {
-// 	// 	"scope": "javascript,typescript",
-// 	// 	"prefix": "log",
-// 	// 	"body": [
-// 	// 		"console.log('$1');",
-// 	// 		"$2"
-// 	// 	],
-// 	// 	"description": "Log output to console"
-// 	// }
-// }
+  final List<Contact> _contacts = [];
+
+  int get length => _contacts.length;
+
+  void add({required Contact contact}) => _contacts.add(contact);
+
+  void remove({required Contact contact}) => _contacts.remove(contact);
+
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
+}
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a new contact"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+                hintText: 'Enter a new contact name here...'),
+          ),
+          TextButton(
+              onPressed: () {
+                final contact = Contact(name: _controller.text);
+                ContactBook().add(contact: contact);
+                Navigator.pop(context);
+              },
+              child: const Text('Add contact'))
+        ],
+      ),
+    );
+  }
+}
